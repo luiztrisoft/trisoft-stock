9bbfa9dad5f4fe19de245a85386e6f9cc14bf8ba
user <luiz.trisoft@gmail.com>
2021-06-29 22:16:56 -0300

    Conectando componentes com o Redux

--- src/redux/Products/Products.reducer.ts
+++ src/redux/Products/Products.reducer.ts
@@ -1,10 +1,11 @@
+import Products from "../../shared/Table/Table.mockdata"
 
 export interface Action<T = any> {
     type: string
     payload?: T
 }
 
-export default function (state = [], action: Action){
+export default function (state = Products, action: Action){
     switch(action.type){
         case 'INSERT_NEW_PRODUCT':
             return [...state, action.payload]
@@ -12,4 +13,4 @@ export default function (state = [], action: Action){
             return state
     }
 
-}
+} 

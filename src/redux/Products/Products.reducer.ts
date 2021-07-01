e36554749919eb438e332962f8671dd38b637bb0
user <luiz.trisoft@gmail.com>
2021-06-30 23:39:33 -0300

    Redux Thunk

--- src/redux/Products/Products.reducer.ts
+++ src/redux/Products/Products.reducer.ts
@@ -1,19 +1,16 @@
 import Products, { Product } from "../../shared/Table/Table.mockdata"
-
-export interface Action<T = any> {
-    type: string
-    payload?: T 
-}
+import { Action } from ".."
 
 export default function (state = Products, action: Action): Product[] {
-    switch(action.type){
-        case 'INSERT_NEW_PRODUCT':
-            return [...state, {
-                ...action.payload,
-                _id: String(state.length + 1)
-              }]
-        default:
-            return state
-    }
-
-} 
+  switch (action.type) {
+    case 'FETCH_PRODUCTS':
+      return [...action.payload]
+    case 'INSERT_NEW_PRODUCT':
+      return [...state, {
+        ...action.payload,
+        _id: String(state.length + 1)
+      }]
+    default:
+      return state
+  }
+}
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,25 +1,32 @@
 import {
-    createStore,
-    combineReducers,
-    compose,
-    applyMiddleware
-  } from 'redux'
-  import thunk from 'redux-thunk'
-  import Products from './Products/Products.reducer'
+  createStore,
+  combineReducers,
+  compose,
+  applyMiddleware
+} from 'redux'
+import thunk, { ThunkAction } from 'redux-thunk'
+import Products from './Products/Products.reducer'
 
-  const reducers = combineReducers({
-    products: Products
-  })
-  
-  const store = createStore(
-    reducers,
-    compose(
-      applyMiddleware(thunk),
-      // @ts-ignore
-      ...(window.__REDUX_DEVTOOLS_EXTENSION__ ? [window.__REDUX_DEVTOOLS_EXTENSION__()] : [])
-      //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+const reducers = combineReducers({
+  products: Products
+})
+
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(thunk),
+    // @ts-ignore
+    ...(window.__REDUX_DEVTOOLS_EXTENSION__ ? [window.__REDUX_DEVTOOLS_EXTENSION__()] : [])
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-  
-  export default store
-  
+)
+
+export interface Action<T = any> {
+  type: string
+  payload?: T
+}
+
+export type Thunk<T = any> = ThunkAction<void, typeof reducers, unknown, Action<T>>
+
+export default store
+
